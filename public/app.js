c3627ab6760c2f349aaaf628fc138f8a56ecf321
Micheal Doolittle <mdoolittle@montefiore.org>
2026-08-14 07:53:55 -0400

    Restore stats and insights hover polish

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 ﻿// Animated agent frame FX are retired; production keeps only static frame art.
 
 console.log("SCRIPT START");
-const RANKEDCOACH_APP_BUILD_ID = "20260814-goal-loadout-map-slider-02";
+const RANKEDCOACH_APP_BUILD_ID = "20260814-stats-insights-hover-01";
 globalThis.RankedCoachBuild = Object.freeze({ id: RANKEDCOACH_APP_BUILD_ID });
 
 // ========================
