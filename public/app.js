7d7c5e91e9742530c6bd53da5446fdb16112c66d
Micheal Doolittle <mdoolittle@montefiore.org>
2026-08-14 09:09:43 -0400

    Scale up loadout silhouette

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 ﻿// Animated agent frame FX are retired; production keeps only static frame art.
 
 console.log("SCRIPT START");
-const RANKEDCOACH_APP_BUILD_ID = "20260814-supporting-reads-overflow-01";
+const RANKEDCOACH_APP_BUILD_ID = "20260814-silhouette-scale-01";
 globalThis.RankedCoachBuild = Object.freeze({ id: RANKEDCOACH_APP_BUILD_ID });
 
 // ========================
